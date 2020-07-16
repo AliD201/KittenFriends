3ad01348408250ef8173140ced72afdcd7c44056
AliD201 <AliD201@users.noreply.github.com>
2020-07-16 21:51:17 +0300

    Updates

---
+++ precache-manifest.ef1926c9d9e50b19678f8eff566d2fb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d357cf18371635396c51388df9d418f5",
+    "url": "/KittenFriends/index.html"
+  },
+  {
+    "revision": "5ab270b0de86236c5af3",
+    "url": "/KittenFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ebd7755e1099446dfe2d",
+    "url": "/KittenFriends/static/css/main.f6f57ef6.chunk.css"
+  },
+  {
+    "revision": "5ab270b0de86236c5af3",
+    "url": "/KittenFriends/static/js/2.b07ce836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KittenFriends/static/js/2.b07ce836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd7755e1099446dfe2d",
+    "url": "/KittenFriends/static/js/main.420b80bf.chunk.js"
+  },
+  {
+    "revision": "299a3563dd214957e444",
+    "url": "/KittenFriends/static/js/runtime-main.4d3c1551.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/KittenFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
